08db539a0d75a5905e728b191be76ab8fbae47ac
estefanymoreira <estefanymoreira06@gmail.com>
2024-08-20 14:38:47 -0300

    modificado el listado en js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
 
-const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
+let container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 
 /**
  * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
@@ -15,7 +15,39 @@ function showData(dataArray) {
   for (const item of dataArray) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+  console.log(dataArray);
   }
 }
 
+
+
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  fetch("json/data.json")
+  .then(response => response.json())
+  .then(data =>{console.log(data.students);
+    showData(data.students);
+  })
+});
+
+
+
+
+/*
+*Intentos fallidos
+const arreglo = data.json.parse(dataArray);
+async function showData() {
+  try {
+    const dataArray = await getData();
+    for (const item of dataArray) {
+      container.innerHTML += `<p>${item.name} ${item.lastname}</p>`;
+    }
+  } catch (error) {
+    console.error("Error showing data:", error);
+  }
+}
+  fetch("json/data.json")
+  .then(response => response.json())
+  .then(data => showData(DATA_URL)) 
+  */
